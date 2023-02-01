4d0e63999ff75b0045c3f896f902fcfd8f3a8039
motez-baccouch <80569257+motez-baccouch@users.noreply.github.com>
2023-02-01 10:55:49 +0100

    certif fix

--- utils/Authentification.py
+++ utils/Authentification.py
@@ -1,20 +1,48 @@
 from getpass import getpass
 import hashlib
+import json
 import re
+import socket
 import sqlite3
 from tabnanny import check
+from tkinter import messagebox
 from utils.Utilisateur import Utilisateur
+import ssl
 
 
 class Authentification:
     def __init__(self):
+        
+        """ context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+
+        context.load_cert_chain(certfile="path/to/certificate.pem", keyfile="chatroom_keys/server_key.pem")
+        
+        with socket.create_connection(("127.0.0.1", 443)) as sock:
+            with context.wrap_socket(sock, server_hostname="example.com") as ssock:
+                # Send registration details over the SSL connection
+                ssock.sendall(json.dumps({"username": username, "password": password}).encode())
+                response = json.loads(ssock.recv(1024).decode())
+                if response["status"] == "success":
+                # Open a connection to the SQLite3 database
+                    self.connection = sqlite3.connect("utilisateurs_bd.db")
+                    self.cursor = self.connection.cursor()
+                    self.cursor.execute(
+                    "SELECT name FROM sqlite_master WHERE type='table' AND name='UTILISATEUR';")
+                    if (self.cursor.fetchone()[0] != 'UTILISATEUR'):
+                     print("Pas de compte trouvé.")
+                    exit()
+                else:
+                    messagebox.showerror("Signup", "Invalid certificate") """
+        
         self.connection = sqlite3.connect("utilisateurs_bd.db")
         self.cursor = self.connection.cursor()
         self.cursor.execute(
-            "SELECT name FROM sqlite_master WHERE type='table' AND name='UTILISATEUR';")
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='UTILISATEUR';")
         if (self.cursor.fetchone()[0] != 'UTILISATEUR'):
             print("Pas de compte trouvé.")
             exit()
+        
+        
 
     def authentifier(self,username,password):
         utilisateur = Utilisateur()
@@ -43,14 +71,11 @@ class Authentification:
         if (self.cursor.fetchall() == []):
             
             return False,"Credentials invalides."
-        ##token=input("Entrer le token secret associe a ce compte:\n> ")
-        ##if (not self.verifier_token(token,utilisateur.username)):
-        ##    print("Credentials invalides.")
-        ##    return False, None
+       
 
         self.cursor.execute(sql_query, utilisateur_a_trouver)
         utilisateur_trouve=self.cursor.fetchone()
-        return True, "welcome to our chatbot"
+        return True, "welcome to our trinodchatbot"
 
     def verifier_token(self, token, username):
         sql_query='SELECT * FROM utilisateur WHERE token=? and username=?'
